db5863e8cd6f3fe4b1d727c1c16f25da0c26a483
Jazastry <yogib@mail.bg>
2014-12-29 20:33:52 +0200

    Added filter by category - All Ads View.

--- OnlineMarket/app/js/controllers/MainController.js
+++ OnlineMarket/app/js/controllers/MainController.js
@@ -1,10 +1,19 @@
 app.controller('MainController', function($scope, $http){
 	$scope.currentPage = "Home";
+	$scope.cat = undefined;
 	$http.get('http://localhost:1337/api/Ads?PageSize=100')
 		.success(function(data){			
 			$scope.ads = data;
 			console.log($scope.ads);
 		});
+	var filter = function(category){
+		if (category !== undefined) {
+			$scope.cat = parseInt(category);
+		} else {
+			$scope.cat = undefined;
+		}		
+	};
+	$scope.filter = filter;
 });
 
 // GET api/Ads?CategoryId={CategoryId}&TownId={TownId}&StartPage={StartPage}&PageSize={PageSize}
